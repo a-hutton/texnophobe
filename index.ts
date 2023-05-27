367e006561e84960db7e069cdfff945aec67563e
Adam Hutton <adam1.hutton1@gmail.com>
2023-05-27 21:42:08 +0100

    makeLatex function exported

--- index.ts
+++ index.ts
@@ -1,10 +1,3 @@
-import { CharStream, CommonTokenStream } from "antlr4";
-import TexnophobeLexer from "./parser/generated/TexnophobeLexer.js";
-import TexnophobeParser from "./parser/generated/TexnophobeParser.js";
-import { LatexVisitor } from "./parser/LatexVisitor.js";
-import { generateLatex } from "./generator.js";
+import { generateLatex } from "./generator";
 
-const input = `x := (-b +- _/(b^2-4ac))/(2a)`;
-const result = generateLatex(input);
-
-console.log(result);
+export const makeLatex = generateLatex;
